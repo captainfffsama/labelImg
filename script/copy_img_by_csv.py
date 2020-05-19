2694c6c3fc4b247c363ccd0c9abfd7cd78ea616c
lijun <874576138@qq.com>
2020-05-19 22:21:28 +0800

    script

---
+++ script/copy_img_by_csv.py
@@ -0,0 +1,24 @@
+'''
+@Author: lijun
+@Date: 2020-05-15 16:48:56
+@LastEditors: lijun
+@LastEditTime: 2020-05-15 18:43:36
+@Description: hang zhou test ,analyse num error data,copy error image to train
+'''
+import sys
+sys.path.append('..')
+from util.read_file import Readcsv
+from util.deal_file import Dealfile
+# from project.util.read_csv import Readcsv
+# from project.util.deal_file import Dealfile
+if __name__ == "__main__":
+    csv_path = r'D:\cbv7700\bin\result_all.csv'
+    target_path = r'Y:\data\num\unsb'
+    readcsv = Readcsv()
+    dealfile = Dealfile()
+    csv_datas = readcsv.read_csv(csv_path)
+    # print(csv_datas)
+    for row_data in csv_datas:
+        if "电流数码表" in row_data[2]:
+            print(row_data[7])
+            dealfile.copy_file(row_data[7],target_path)
