43bfdbeb45f43ec7ff31080bbb659f171577578a
lijun <874576138@qq.com>
2020-06-01 18:37:06 +0800

    增加划分数据集

---
+++ script/divide_data.py
@@ -0,0 +1,88 @@
+'''
+@Author: lijun
+@Date: 2020-06-01 11:58:08
+@LastEditors: lijun
+@LastEditTime: 2020-06-01 18:32:56
+@Description: file content
+'''
+import os
+import shutil
+import random
+from concurrent import futures
+from tqdm import tqdm
+from collections import defaultdict
+from statics_classnum import static_class_num
+from util.deal_file import Dealfile
+deal_file = Dealfile()
+class divideDataTT(object):
+    def __init__(self,data_map):
+        global divide_data
+        divide_data = {}
+        for key,value in data_map.items():
+            divide_data[os.path.basename(key)] = value
+            if os.path.exists(key):
+                shutil.rmtree(key)
+                os.mkdir(key)
+            else:
+                os.mkdir(key)
+
+
+    def divide_data_tt(self,ori_path,target_path):
+        class_num,path_class_name,all_paths = static_class_num(ori_path)
+        train_class_num = {}
+        test_class_num = {}
+        val_class_num = {}
+        divide_class = {}
+        train_filter_data = defaultdict(int)
+        test_filter_data = defaultdict(int)
+        global divide_data
+        for class_num_data in class_num:
+            train_class_num[class_num_data[0]] = round(class_num_data[1]*divide_data['train'])
+            test_class_num[class_num_data[0]] = round(class_num_data[1]*divide_data['test'])
+            val_class_num[class_num_data[0]] = round(class_num_data[1]*divide_data['val'])
+        print(train_class_num,test_class_num,val_class_num)
+        random.shuffle(all_paths)
+        # for class_num in divide_class_num:
+        
+        for path in all_paths:
+            for class_name,classs_count in path_class_name[path].items():
+                if(train_class_num[class_name]>train_filter_data[class_name]):
+                    train_filter_data[class_name]+=classs_count
+                    is_train = True
+                    is_test = False
+                    is_val = False
+                elif(test_class_num[class_name]>test_filter_data[class_name]):
+                    test_filter_data[class_name]+=classs_count
+                    is_train = False
+                    is_test = True
+                    is_val = False
+                else:
+                    is_train = False
+                    is_test = False
+                    is_val = True
+            jpg_path = path.replace('xml','jpg')
+            if is_train:
+                divide_class.setdefault('train',[]).append(path)
+                divide_class.setdefault('train',[]).append(jpg_path)
+            elif is_test:
+                divide_class.setdefault('test',[]).append(path)
+                divide_class.setdefault('test',[]).append(jpg_path)
+            elif is_val:
+                divide_class.setdefault('val',[]).append(path)
+                divide_class.setdefault('val',[]).append(jpg_path)
+        for key,value in divide_class.items():
+            new_target_path = os.path.join(target_path,key)
+            with futures.ThreadPoolExecutor(32) as pool:
+                task_list = (pool.submit(deal_file.copy_file,file_path,new_target_path) for file_path in value)
+                for i in tqdm(futures.as_completed(task_list),total=len(value)):
+                    pass    
+if __name__ == "__main__":
+    base_path = r'C:\Users\lj893\Desktop\test'
+    target_path = r'C:\Users\lj893\Desktop\test'
+    data_map = {
+        os.path.join(target_path,'train'):0.7,
+        os.path.join(target_path,'test'):0.2,
+        os.path.join(target_path,'val'):0.1
+    }
+    dividedata = divideDataTT(data_map)
+    dividedata.divide_data_tt(base_path,target_path)
--- script/statics_classnum.py
+++ script/statics_classnum.py
@@ -2,7 +2,7 @@
 @Author: lijun
 @Date: 2020-05-28 16:52:43
 @LastEditors: lijun
-@LastEditTime: 2020-05-29 14:57:02
+@LastEditTime: 2020-06-01 15:02:12
 @Description: file content
 '''
 from util import read_file
@@ -21,6 +21,7 @@ def analyse_num_by_xml(xml_path,filter_name,is_filter,work_num):
     all_paths = comm_function.traverse_files(xml_path,filter_name,is_filter)
     all_names = []
     all_names_dict = defaultdict(int)
+    all_path_name_dict = {}
     couter = 0
     # for path in tqdm(all_paths):
     #     xml_datas = read_xml.read_xml(path)
@@ -33,27 +34,32 @@ def analyse_num_by_xml(xml_path,filter_name,is_filter,work_num):
     #     # all_names.append(xml_names)#!全部存储，数据量较大
     # print(all_names_dict)
     with futures.ThreadPoolExecutor(work_num) as pool:
-        task_list = (pool.submit(deal_xml,path,all_names_dict) for path in all_paths)
+        task_list = (pool.submit(deal_xml,path,all_names_dict,all_path_name_dict) for path in all_paths)
         for i in tqdm(futures.as_completed(task_list),total=len(all_paths)):
             pass
     all_names_dict = sorted(all_names_dict.items(),key=lambda item: item[1])
     for all_names in all_names_dict:
         print('{:20s}---{:>10s}'.format(repr(all_names[0]),repr(all_names[1])))
-    # print(all_names_dict)
-def deal_xml(path,all_names_dict):
+    return all_names_dict,all_path_name_dict,all_paths
+def deal_xml(path,all_names_dict,all_path_name_dict):
     xml_datas = read_xml.read_xml(path)
     # print(xml_datas)
     xml_names = list(map(lambda xml_data: xml_data['name'],xml_datas['object']))
     # print(xml_names)
+    one_file_names_dict = defaultdict(int)
     for xml_name in xml_names:
-        # all_names.append(xml_name)
+        one_file_names_dict[xml_name]+=1
         all_names_dict[xml_name]+=1
-if __name__ == "__main__":
+    all_path_name_dict[path] = one_file_names_dict
+def static_class_num(xml_path):
     time_begin = time.time()
-    xml_path = r'Y:\data\all_device\all'
     filter_name = ['xml']
     is_filter = True
     work_num = 32
-    analyse_num_by_xml(xml_path,filter_name,is_filter,work_num)
+    all_names_dict,all_path_name_dict,all_paths = analyse_num_by_xml(xml_path,filter_name,is_filter,work_num)
     time_end = time.time()
     print('程序运行时间:{} s'.format(time_end-time_begin))
+    return all_names_dict,all_path_name_dict,all_paths
+if __name__ == "__main__":
+    xml_path = r'Y:\data\all_device\all'
+    static_class_num(xml_path)
